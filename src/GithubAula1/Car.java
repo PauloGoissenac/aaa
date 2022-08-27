2490e1eb5a3d0cc3244c0ef4520414add0f6270e
Paulo Gois <paulo.gois@alunos.sc.senac.br>
2022-08-26 21:04:01 -0300

    Nova classe Car e nova impressão

---
+++ src/GithubAula1/Car.java
@@ -0,0 +1,13 @@
+package GithubAula1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author paulo.gois
+ */
+public class Car {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+    }
+}
---
+++ src/GithubAula1/GithubAula1.java
@@ -0,0 +1,15 @@
+package GithubAula1;
+
+/**
+ *
+ * @author paulo.gois
+ */
+public class GithubAula1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Welcome to bithub");
+    }
+    
+}
